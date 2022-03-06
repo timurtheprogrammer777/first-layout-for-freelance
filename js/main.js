f021eb3bd791c1a43a23eb6a4f864c7ad188cb1e
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-03-06 16:40:58 +0300

    done

--- js/main.js
+++ js/main.js
@@ -8,4 +8,11 @@ $(function(){
         arrows:true
       });
 
+      $('.portfolio__slider-inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows:true
+      });
+
 });
